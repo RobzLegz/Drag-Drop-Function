7a5049485e82af71ba6ff6adb6ea9d759b4a7df7
Robzlegz <robzlegz@gmail.com>
2021-02-19 13:25:46 +0200

    when dragged replace the container

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,12 @@ import Droppable from './Droppable';
 
 function App() {
 
-  const [isDropped, setIsDropped] = useState(false);
+  const [parent, setParent] = useState(null);
+  const containers = ["A","B","C"];
 
   const hendleDragEnd = (event: any) => {
-    if(event.over && event.over.id === "droppable"){
-      setIsDropped(true);
-    }
+    const {over} = event;
+    setParent(over ? over.id : null);
   }
 
   const draggableMarkup = <Draggable><h1>Todo</h1></Draggable>;
@@ -19,10 +19,12 @@ function App() {
   return (
     <div className="app">
       <DndContext onDragEnd={hendleDragEnd}>
-        {!isDropped ? draggableMarkup : null}
-        <Droppable>
-          {isDropped ? draggableMarkup : "Drop here"}
-        </Droppable>
+        {parent === null ? draggableMarkup : null}
+        {containers.map((id) => (
+          <Droppable key={id} id={id}>
+            {parent === id ? draggableMarkup : <h1>Drop Here</h1>}
+          </Droppable>
+        ))}
       </DndContext>      
     </div>
   );
--- src/Droppable.tsx
+++ src/Droppable.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Droppable = (props: any) => {
     
     const {isOver, setNodeRef} = useDroppable({
-        id: "droppable",
+        id: props.id,
     });
 
     const style = {
