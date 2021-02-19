7e698fb45592a74f7ee0365ee65279601c449a9d
Robzlegz <robzlegz@gmail.com>
2021-02-19 13:42:42 +0200

    showing images

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,19 @@ import Droppable from './Droppable';
 function App() {
 
   const [parent, setParent] = useState(null);
-  const containers = ["A","B","C"];
+  const containers = ["React","Redux","Typescript"];
+  const logoMap = {
+    "React": "https://miro.medium.com/max/500/1*cPh7ujRIfcHAy4kW2ADGOw.png",
+    "Redux": "https://seeklogo.com/images/R/redux-logo-9CA6836C12-seeklogo.com.png",
+    "Typescript": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/512px-Typescript_logo_2020.svg.png",
+  }
 
   const hendleDragEnd = (event: any) => {
     const {over} = event;
     setParent(over ? over.id : null);
   }
 
-  const draggableMarkup = <Draggable><h1>Todo</h1></Draggable>;
+  const draggableMarkup = <Draggable><h3>Drag to show more info</h3></Draggable>;
 
   return (
     <div className="app">
@@ -22,7 +27,12 @@ function App() {
         {parent === null ? draggableMarkup : null}
         {containers.map((id) => (
           <Droppable key={id} id={id}>
-            {parent === id ? draggableMarkup : <h1>Drop Here</h1>}
+            {parent === id ? 
+              <div className="app-selected">
+                <img src={logoMap[parent!]} alt=""/>
+                <h2>You selected {parent}</h2> 
+                {draggableMarkup}                
+              </div> : <h1>{id}</h1>}
           </Droppable>
         ))}
       </DndContext>      
